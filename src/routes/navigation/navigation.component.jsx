51d3564f26cda4f201e47e19198c997053b662da
naresh-webdev <gkmnaresh01@gmail.com>
2022-10-06 15:07:36 +0530

    addd logo component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link to={"/"} className="logo-container">
-          <div>Logo</div>
+          <CrownLogo className="logo" />
         </Link>
 
         <div className="links-container">
