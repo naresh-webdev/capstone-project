7fd1544ff55446e14ef5e222d58cebeb8782f075
naresh-webdev <gkmnaresh01@gmail.com>
2022-10-07 21:05:57 +0530

    firebase auth implemented

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from "../../utilities/firebase/firebase.utillities";
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>sign in with Google popup</button>
     </div>
   );
 };
--- src/utilities/firebase/firebase.utillities.js
+++ src/utilities/firebase/firebase.utillities.js
@@ -27,3 +27,6 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account",
 });
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
