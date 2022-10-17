06d5733cd1a4fdd6bbc7de5bd8606263c666ed54
naresh-webdev <gkmnaresh01@gmail.com>
2022-10-17 15:54:58 +0530

    cart-icon changed to styled compoennt

--- src/component/cart-icon/cart-icon.component.jsx
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -1,7 +1,11 @@
-import "./cart-icon.styles.scss";
+import {
+  ShoppingIcon,
+  CartIconContainer,
+  ItemCount,
+} from "./cart-icon.styles.jsx";
 
 import { CartContext } from "../../contexts/cart.context";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { ReactComponent as Shoppingsvg } from "../../assets/shopping-bag.svg";
 import { useContext } from "react";
 
 const CartIcon = () => {
@@ -10,10 +14,10 @@ const CartIcon = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
-      <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartCountItem}</span>
-    </div>
+    <CartIconContainer onClick={toggleIsCartOpen}>
+      <ShoppingIcon />
+      <ItemCount>{cartCountItem}</ItemCount>
+    </CartIconContainer>
   );
 };
 
